ff374694d98929774ae0100fb35245f65d0f554a
MattBussing <mbussing@mymail.mines.edu>
2018-12-05 12:16:38 -0700

    fixed error

--- mysql_connection.php
+++ mysql_connection.php
@@ -1,8 +1,8 @@
 <?php
     $user = "root";
     $password = "";
-    if(file_exists('matt.php'))
     $db = "csci445_final";
+    if(file_exists('matt.php'))
     include 'matt.php';
     $conn = new mysqli("localhost", $user, $password, $db) or die("You are not connected");
 
