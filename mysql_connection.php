5ad5f77ecfadab0df3eedf413c622fd573f672d0
MattBussing <mbussing@mymail.mines.edu>
2018-12-05 11:56:53 -0700

    cleaned up and added my password

--- mysql_connection.php
+++ mysql_connection.php
@@ -1,6 +1,8 @@
 <?php
     $user = "root";
     $password = "";
+    if(file_exists('matt.php'))
+    include 'matt.php';
     $db = "csci445_final";
     $conn = new mysqli("localhost", $user, $password, $db) or die("You are not connected");
 
