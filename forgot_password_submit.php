aa32c00007ef322610c19fb8b3fa0ec5e1c4b1f4
Luke Pierce <lpierce1313@gmail.com>
2018-12-05 10:56:32 -0700

    new

---
+++ forgot_password_submit.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <?php
+      include 'head.php';
+    ?>
+  </head>
+  <body>
+    <?php
+      $home = 0;
+      include 'header.php';
+    ?>
+    <hr>
+      <section class="center form">
+        <div class="has-text-centered">
+          <h1 class="is-size-3">Welcome to Task Manager <i class="fa fa-tasks" aria-hidden="true" id="taskIcon"> </i></h1>
+          <h3 class="is-size-4">Please login below</h3>
+          <br> <br> <br>
+        </div>
+        <div class="form_holder_login">
+          <p>
+            <?php
+              if(isset($error)) {
+                echo $error;
+              }
+            ?>
+
+          </p>
+          <form action="login_submit.php" method="post">
+            <div class="field">
+              <label class="label">Email</label>
+              <div class="control has-icons-left has-icons-right">
+                <input class="input" type="email" placeholder="Email" name="email">
+                <span class="icon is-small is-left">
+                  <i class="fas fa-envelope"></i>
+                </span>
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Password</label>
+              <div class="control">
+                <input class="input" type="password" name="password">
+              </div>
+            </div>
+            <a class="button is-link is-inverted">Forgot Password</a>
+            <input class="button is-primary login_button" type="submit" title="Login">
+            </div>
+          </form>
+          <br> <br> <br>
+        </div>
+      </section>
+      <br>
+      <?php
+        include 'footer.php';
+      ?>
+  </body>
+</html>
