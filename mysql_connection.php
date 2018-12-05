f85048c67b9a8efc361155ce963a4c462ba5c351
Alex DeGroat <adegroat@mymail.mines.edu>
2018-12-05 12:18:38 -0700

    Add session expiration

--- mysql_connection.php
+++ mysql_connection.php
@@ -7,6 +7,14 @@
     session_start();
 
     function loggedIn() {
-    	return isset($_SESSION['currentUser']);
+    	return isset($_SESSION['currentUser']) && isset($_SESSION['loginTime']);
+    }
+
+    // Log out the current user if their session expires
+    $MAX_SESSION_TIME = 60*60;
+    if(loggedIn()) {
+    	if(time() - (int)$_SESSION['loginTime'] > $MAX_SESSION_TIME) {
+    		header("Location: logout.php");
+    	}
     }
 ?>
