8affad6f690b8699054274b8ac643a8a1dc2168f
MattBussing <mbussing@mymail.mines.edu>
2018-12-05 12:49:17 -0700

    added id to db

--- dbcreation.sql
+++ dbcreation.sql
@@ -1,8 +1,7 @@
 DROP TABLE IF EXISTS `users`;
 DROP TABLE IF EXISTS `tasks`;
 
-
-CREATE TABLE `users` ( `email` VARCHAR(255) NOT NULL , `first_name` VARCHAR(255) NOT NULL , `last_name` VARCHAR(255) NOT NULL , `password` VARCHAR(255) NOT NULL , PRIMARY KEY (`email`)) ENGINE = InnoDB;
+CREATE TABLE `users` ( `id` INT NOT NULL AUTO_INCREMENT , `email` VARCHAR(255) NOT NULL , `first_name` VARCHAR(255) NOT NULL , `last_name` VARCHAR(255) NOT NULL , `password` VARCHAR(255) NOT NULL , PRIMARY KEY (`email`)) ENGINE = InnoDB;
 
 CREATE TABLE `tasks` ( `id` INT NOT NULL AUTO_INCREMENT , `user_id` INT NOT NULL , `name` VARCHAR(255) NOT NULL , `description` TEXT NOT NULL , `due_date` DATE NOT NULL , `urgency` INT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 
